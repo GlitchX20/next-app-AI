f2c472891206bde8c6c856a8b32b047f3dc0fb84
Zarif <zarifhosen2015@gmail.com>
2023-09-12 01:07:39 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The best journal app, period.</h1>
+        <p className="text-2xl mb-4 text-white/70">
+          This app here is the best for tracking you mood throughout the day!
+          All you have to do is be a little honest.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-md">
+                Get started
+              </span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
