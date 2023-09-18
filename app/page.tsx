2da3edd0c398907dc43c9d67d763f85a53457eb7
Zarif <zarifhosen2015@gmail.com>
2023-09-18 08:37:11 +0600

    Added Analysis model and related changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from '@clerk/nextjs/server'
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -10,7 +15,7 @@ export default function Home() {
           All you have to do is be a little honest.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-md">
                 Get started
