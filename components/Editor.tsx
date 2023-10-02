297ad3a808c6dc9bb50f284cf7c29ffe4013a4cf
Zarif <zarifhosen2015@gmail.com>
2023-10-02 13:05:38 +0600

    Add textarea to Editor component This commit adds a textarea to the Editor component.

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,7 +1,19 @@
 'use client'
+import { useState } from 'react'
 
 const Editor = (entry) => {
-  return <div>{entry.content}</div>
+  const [value, setValue] = useState(entry.content)
+  return (
+    <div className="h-[calc(100vh-60px)] outline-none">
+      <div className="h-full w-full px-6 flex items-center justify-end outline-none">
+        <textarea
+          className="w-full h-full p-8 text-xl outline-none"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Editor
